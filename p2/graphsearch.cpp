02eab97c954c8f9235ab55f7a0a5e626e9c4b10e
Jorge <jogonzal@microsoft.com>
2016-10-22 15:07:42 -0700

    Single threaded implementation done

--- p2/graphsearch.cpp
+++ p2/graphsearch.cpp
@@ -52,33 +52,91 @@ Node* GetOrCreateNode(map<unsigned long long, Node*> *graphInputMap, unsigned lo
 	}
 }
 
+void PrintToFileAndConsole(int verticesTotal, int edgesTotal, unsigned long long rootValue, int vertexCount, int maxLevel){
+	printf("\n\nGraph vertices: %d with total edges %d. Reached vertices from %llx is %d and max level is %d.\n\n",
+		verticesTotal, edgesTotal, rootValue, vertexCount, maxLevel);
+		
+	FILE *f = fopen("output.txt", "w+");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    
+    fprintf(f, "Graph vertices: %d with total edges %d. Reached vertices from %llx is %d and max level is %d.\n\n",
+		verticesTotal, edgesTotal, rootValue, vertexCount, maxLevel);
+    
+	printf("I wrote this to output.txt\n");
+	
+    fclose(f);
+}
+
+void SingleThreadedBfs(int *maxLevel, int *vertexCount, int *edgeCount, Node *root){
+	queue<Node*> *bfsQueue = new queue<Node*>();
+	bfsQueue->push(root);
+	while(bfsQueue->size() > 0){
+		printf("Level %d has %lu elements to explore. Exploring...\n", *maxLevel, bfsQueue->size());
+		
+		queue<Node*> *newQueue = new queue<Node*>();
+		
+		// Ennumerate through children of elements in the queue
+		while(bfsQueue->size() > 0){
+			Node *nodeToExplore = bfsQueue->front();
+			bfsQueue->pop();
+			
+			(*edgeCount)++; // Edges have to be counted, even if we already visited that node
+
+			// Skip if already visited
+			if (!nodeToExplore->visited){
+				(*vertexCount)++;
+				
+				// Push all children
+				for (vector<Node*>::iterator it = nodeToExplore->children->begin() ; it != nodeToExplore->children->end(); ++it){
+					Node *child = *it;
+					child->previous = nodeToExplore; // For shortest path
+					newQueue->push(child);
+				}
+				
+				// Mark as visited
+				nodeToExplore->visited = true;
+			}
+		}
+		
+		free(bfsQueue);
+		bfsQueue = newQueue;
+		(*maxLevel)++;
+	}
+	
+	free(bfsQueue);
+}
+
 int main(int argc, char* argv[])
 {
-    if (argc != 3) {
-        printf("usage: %s threads target\n", argv[0]);
-		printf("Sample target: 26849");
+    if (argc != 4) {
+        printf("usage: %s threads root inputfile\n", argv[0]);
+		printf("Sample root: 0");
         return -1;
     }
 	
 	int threadCount = atoi(argv[1]);
 	unsigned long long rootValue = (unsigned long long)atoi(argv[2]);
+	char* fileName = argv[3];
 	
-	printf("Starting. ThreadCount %d, root %llx\n", threadCount, rootValue);
-    char const* const fileName = "graphinput.bin";
+	printf("Starting. ThreadCount %d, root %llx, inputfile %s\n", threadCount, rootValue, fileName);
     FILE* file = fopen(fileName, "r");
 
     int buffSize = 100;
 
     struct edge *buff = (struct edge *) malloc(sizeof(struct edge) * buffSize);
     
-    int edgesRead = 0;
+    int edgesTotal = 0;
  
 	map<unsigned long long, Node*> *graphInputMap = new map<unsigned long long, Node*>();
 
 	
     for (;;) {
         size_t elementsRead = fread(buff, sizeof(struct edge), buffSize, file);
-		edgesRead += elementsRead;
+		edgesTotal += elementsRead;
         for(int i = 0; i < elementsRead; i++){
             struct edge edge = buff[i];
 			Node *nodeOrigin = GetOrCreateNode(graphInputMap, edge.origin);
@@ -101,9 +159,11 @@ int main(int argc, char* argv[])
 		printf("Where did you get this value from? The root you specified wasn't in the input file\n");
 	}
 	
-	printf("I read %d edges and have found %lu vertices (in total). Now doing breadth first search to calculate the number of edges and vertices from root \n",
-				edgesRead, graphInputMap->size());
-				
+	int verticesTotal = graphInputMap->size();
+	
+	printf("I read %d edges and have found %d vertices (in total). Now doing breadth first search to calculate the number of edges and vertices from root %llx \n",
+				edgesTotal, verticesTotal, rootValue);
+
 	free(graphInputMap);
 	free(buff);
 	
@@ -112,44 +172,10 @@ int main(int argc, char* argv[])
 	int maxLevel = 0; // Assumming at least root exists, level is 1
 	int vertexCount = 0;
 	int edgeCount = 0;
-	queue<Node*> *bfsQueue = new queue<Node*>();
-	bfsQueue->push(root);
-	while(bfsQueue->size() > 0){
-		printf("Level %d has %lu elements to explore. Exploring...\n", maxLevel, bfsQueue->size());
-		
-		queue<Node*> *newQueue = new queue<Node*>();
-		
-		// Ennumerate through children of elements in the queue
-		while(bfsQueue->size() > 0){
-			Node *nodeToExplore = bfsQueue->front();
-			bfsQueue->pop();
-			
-			edgeCount++; // Edges have to be counted, even if we already visited that node
-
-			// Skip if already visited
-			if (!nodeToExplore->visited){
-				vertexCount++;
-				
-				// Push all children
-				for (vector<Node*>::iterator it = nodeToExplore->children->begin() ; it != nodeToExplore->children->end(); ++it){
-					Node *child = *it;
-					child->previous = nodeToExplore; // For shortest path
-					newQueue->push(child);
-				}
-				
-				// Mark as visited
-				nodeToExplore->visited = true;
-			}
-		}
-		
-		free(bfsQueue);
-		bfsQueue = newQueue;
-		maxLevel++;
-	}
-
-	printf("Found %d vertices, %d maxLevel, %d edges.\n", vertexCount, maxLevel, edgeCount);
 	
-	free(bfsQueue);
+	SingleThreadedBfs(&maxLevel, &vertexCount, &edgeCount, root);
+
+	PrintToFileAndConsole(verticesTotal, edgesTotal, rootValue, vertexCount, maxLevel);
 
     return 0;
 }
